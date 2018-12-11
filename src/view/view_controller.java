6d57bb3a584d5cd1fa2d770741bf34250290d0d2
Florian <Florian@pc-Florian>
2018-12-11 15:40:56 +0100

    suite

---
+++ src/view/view_controller.java
@@ -0,0 +1,14 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class view_controller {
+	
+@FXML
+private Button BoutonTest;
+@FXML
+void test() {
+	System.out.println("coucou");
+}
+}
